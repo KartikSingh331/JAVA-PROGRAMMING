513109288c9f4d0b5dcd721017ed5fa479caa11c
KartikSingh331 <kartiksinh551@gmail.com>
2025-08-18 23:23:51 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,23 @@
+package Pattern;
+import java.util.Scanner;
+public class Numbers {
+    public static void main(String[] args) {
+//        for (int i=1;i<=4;i++){
+//            for (int j=1;j<=4;j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+        //NEXT QUESTION
+
+        for (int i=1;i<=4;i++) {
+            for (int j = 1; j <= 4; j++) {
+                char ch = 'A';
+                System.out.print(ch);
+
+            }
+            System.out.println();
+        }
+        }
+    }
