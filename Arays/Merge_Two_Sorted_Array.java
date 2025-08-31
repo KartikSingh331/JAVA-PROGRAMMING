9b19fae42b1f5549f610a1e3ac40ee1a40bc7d5f
KartikSingh331 <kartiksinh551@gmail.com>
2025-08-31 21:41:42 +0530

    Add files via upload

---
+++ Arays/Merge_Two_Sorted_Array.java
@@ -0,0 +1,41 @@
+package Arays;
+
+public class Merge_Two_Sorted_Array {
+    public static void main(String[] args) {
+        int[] a = {12, 18, 24, 39};
+        int[] b={14,27,33,54};
+        int[] c=new int[a.length+b.length];
+        int i=0,j=0,k=0;
+        while (i< a.length && j<b.length){
+            if(a[i]<=b[j]){
+                c[k]=a[i];
+                i++;k++;
+            }
+            else {
+                c[k]=b[j];
+
+                j++;k++;
+            }
+        }
+        if (i==a.length){
+            while (j<b.length) {
+                c[k] = b[j];
+                j++;
+                k++;
+            }
+        }
+        else {
+            while (i < a.length) {
+                c[k] = a[i];
+                i++;
+                k++;
+            }
+
+
+        }
+        for (int ele:c) {
+            System.out.print(ele+" ");
+
+        }
+    }
+}
