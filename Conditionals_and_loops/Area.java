95ecbbd5cf4623b4d9c5e642becf01d4a175990a
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-03 20:22:40 +0530

    Add files via upload

---
+++ Conditionals_and_loops/Area.java
@@ -0,0 +1,20 @@
+package Conditionals_and_Loops;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    Float l=sc.nextFloat();
+    Float b=sc.nextFloat();
+    Float area=l*b;
+    Float peri=2*(l+b);
+    if(area>peri){
+        System.out.println("AREA IS GREATER");
+    } else if (peri>area) {
+        System.out.println("Perimeter is larger");
+    }
+    else {
+        System.out.println("Area and perimeter both are equal");
+    }
+
+    }
+}
