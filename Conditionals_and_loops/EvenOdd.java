d140bac902d64c1977f17cfd89d310ba3c513f1f
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-09 22:42:28 +0530

    Add files via upload

---
+++ Conditionals_and_loops/EvenOdd.java
@@ -0,0 +1,16 @@
+package Conditionals_and_Loops;
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int n= sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number");
+        }
+    }
+    }
+
