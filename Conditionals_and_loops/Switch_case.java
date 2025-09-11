54d25f4300842353ff88ac0b0c8bd3f5bd6d11f1
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-11 10:54:52 +0530

    Add files via upload

---
+++ Conditionals_and_loops/Switch_case.java
@@ -0,0 +1,34 @@
+package Conditionals_and_Loops;
+import java.util.Scanner;
+public class Switch_case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+
+        char op=sc.next().charAt(0);
+        int b =sc.nextInt();
+//        if(op=='+') System.out.println(a+b);
+//        if(op=='-') System.out.println(a-b);
+//        if(op=='*') System.out.println(a*b);
+//        if(op=='/') System.out.println(a/b);
+        /*REPLACED BY SWITCH CASE
+         */
+        switch (op){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                System.out.println(a-b);
+                break;
+            case '*':
+                System.out.println(a*b);
+                break;
+            case '/':
+                System.out.println(a/b);
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+
+    }
+}
