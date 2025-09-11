3cb6af696c1f21d330c34f8e87f4bf7dcb1334df
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-11 10:54:25 +0530

    Add files via upload

---
+++ Conditionals_and_loops/QUESTION.java
@@ -0,0 +1,16 @@
+package Conditionals_and_Loops;
+import java.util.Scanner;
+public class QUESTION {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        int d=0;
+        while(n!=0) {
+            n=n/10;
+            d=d+1;
+        }
+        System.out.println(d);
+    }
+
+}
