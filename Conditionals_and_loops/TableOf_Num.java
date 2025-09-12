6b1416249d13ad66bdf8556caff0ecbdf34a6951
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-12 11:59:43 +0530

    Add files via upload

---
+++ Conditionals_and_loops/TableOf_Num.java
@@ -0,0 +1,17 @@
+package Conditionals_and_Loops;
+import java.util.Scanner;
+public class TableOf_Num {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       /* System.out.print("Enter a number: ");
+        int n=sc.nextInt();
+        for (int i=1;i<=10;i++){
+            System.out.println(n*i);
+        }*/
+
+        // SECOND CHUTIYA METHOD BY SIR
+        for (int i=19;i<=190;i+=19){
+            System.out.println(i);
+        }
+    }
+}
