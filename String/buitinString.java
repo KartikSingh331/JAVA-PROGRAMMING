b4bcb8d86a8308745d6e1f8ccfd04282ed193af8
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-14 22:05:59 +0530

    Add files via upload

---
+++ String/buitinString.java
@@ -0,0 +1,11 @@
+package String;
+import java.util.*;
+public class buitinString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s="Kartik is a good boy";
+        System.out.println(s.charAt(12));
+        int n=s.length();
+        System.out.println(n);
+    }
+}
