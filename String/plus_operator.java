635f8c220a552ea898e33ae097834080e663d6e9
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-18 00:29:22 +0530

    Add files via upload

---
+++ String/plus_operator.java
@@ -0,0 +1,12 @@
+package String;
+
+public class plus_operator {
+    public static void main(String[] args) {
+        String s="abc";
+        String a="xyz";
+        s=s+a; // also done s=s+10
+        System.out.println(s);
+        System.out.println("abtm"+10+20);
+        System.out.println(10+20+"abtm");
+    }
+}
