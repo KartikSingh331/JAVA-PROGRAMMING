683c9503b6a25b8b4345f8c6b4e76be7091095fa
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-21 21:25:55 +0530

    Add files via upload

---
+++ String/StringBuilderFunctions.java
@@ -0,0 +1,10 @@
+package String;
+
+public class StringBuilderFunctions {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Kartik");
+        StringBuilder tb=new StringBuilder("Artik");
+        System.out.println(sb.compareTo(tb));
+        System.out.println(sb.reverse());
+    }
+}
