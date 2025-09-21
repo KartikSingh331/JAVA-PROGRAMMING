bbde7c18de87acdd2d9f41e75e2754ba591bb49e
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-21 21:26:50 +0530

    Add files via upload

---
+++ String/Stringclass.java
@@ -0,0 +1,18 @@
+package String;
+import java.util.*;
+public class Stringclass {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+//       String str="KARTIK";
+//        System.out.print("Hi ");
+//        System.out.print(str);
+        String a=sc.nextLine();
+        for (int i=0;i<1;i++){
+            System.out.print(a);
+
+
+        }
+
+    }
+
+}
