0a3e66b876ffa6b546e39abd69df91cdb1885cee
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-23 21:24:55 +0530

    Add files via upload

---
+++ String/StringCompression.java
@@ -0,0 +1,25 @@
+package String;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        String s="aakkkkhrttttttuyyyyyu";
+        char[] arr=s.toCharArray();
+        String ans="";
+        int i=0; int j=0;
+        while (j<arr.length){
+            if (arr[j]==arr[i]) j++;
+            else{
+                   ans+=arr[i];
+                   int len=j-i;
+                   if (len>1) ans+=len;
+                   i=j;
+            }
+        }
+
+        ans+=arr[i];
+        int len=j-i;
+        if (len>1) ans+=len;
+
+        System.out.println(ans);
+    }
+}
