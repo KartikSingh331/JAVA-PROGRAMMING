1c75518e6d90f3b1c4521649c68fe8ee63b36ec2
KartikSingh331 <kartiksinh551@gmail.com>
2025-09-26 12:39:42 +0530

    Add files via upload

---
+++ Pattern/Advance_starplus.java
@@ -0,0 +1,20 @@
+package Pattern;
+import java.util.Scanner;
+public class Advance_starplus {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m= sc.nextInt();
+        for (int i=1;i<=m;i++){
+            int mid=m/2+1;
+            for (int j=1;j<=m;j++){
+                if (i==mid||j==mid){
+                    System.out.print("*"+" ");
+                }
+                else {
+                    System.out.print(" "+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
