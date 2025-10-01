c419c20c22977517528c42308f68d7e3d0f776a9
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-01 23:10:25 +0530

    Add files via upload

---
+++ Recursion1/comparatorInterface.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.*;
+
+public class comparatorInterface {
+    public static void main(String[] args) {
+        Comparator <Integer> obj=new Comparator<Integer>() {
+            @Override
+            public int compare(Integer i, Integer j) {
+                if (i % 10 > j % 10) return 1;
+                else return -1;
+            }
+        };
+        List <Integer> nums=new ArrayList<>();
+        nums.add(41);
+        nums.add(42);
+        nums.add(43);
+        Collections.sort(nums,obj);
+        System.out.println(nums);
+    }
+}
