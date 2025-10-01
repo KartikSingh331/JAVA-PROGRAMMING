19780d1c1beea82b15fb7fea183de0a742c11606
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-02 00:13:18 +0530

    Add files via upload

---
+++ Recursion1/n_to_1.java
@@ -0,0 +1,15 @@
+package Recursion;
+import java.util.Scanner;
+public class n_to_1 {
+    public static void toone(int n){
+        if (n==0) return ; //base value
+        System.out.println(n); //work
+        toone(n-1); //call
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value: ");
+        int n= sc.nextInt();
+        toone(n);
+    }
+}
