81f973dba465c16f842020e4f37ff05620bb68b2
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-06 11:52:25 +0530

    Add files via upload

---
+++ Conditionals_and_loops/Area_circle.java
@@ -0,0 +1,7 @@
+public class Area_circle {
+    public static void main(String[] args) {
+        int r=4;
+        double area=3.14*(r*r);
+        System.out.println("Area of circle is "+area);
+    }
+}
