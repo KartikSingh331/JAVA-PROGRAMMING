79b7ffc22882074fddd8fef90f6e74002823b984
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-09 22:39:02 +0530

    Add files via upload

---
+++ OOPs concept/Student.java
@@ -0,0 +1,12 @@
+package OOPs;
+
+public class Student {
+    String name;
+  private   int rno;    // Changed from double to int
+    double per;
+    public int getRno(){
+        return rno;
+     }
+     public void setRno(int roll){
+    rno=roll;}
+}
