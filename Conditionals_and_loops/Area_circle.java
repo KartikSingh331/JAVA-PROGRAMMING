739e836eb5e89d4348e079bf346231f968146ce7
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-12 21:31:15 +0530

    Update Area_circle.java
    Changing variable data

--- Conditionals_and_loops/Area_circle.java
+++ Conditionals_and_loops/Area_circle.java
@@ -1,7 +1,8 @@
 public class Area_circle {
     public static void main(String[] args) {
-        int r=4;
+        int r=41;
         double area=3.14*(r*r);
         System.out.println("Area of circle is "+area);
     }
 }
+
