5a10da1df16da45005365283a9d3c76b0e9903eb
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-15 10:21:44 +0530

    Add files via upload

---
+++ Sorting1/InDecreasingOrder_BubbleSort.java
@@ -0,0 +1,27 @@
+package Sorting;
+import java.util.Scanner;
+public class InDecreasingOrder_BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]= sc.nextInt();    //taking input of an array
+        }
+        System.out.println();
+        for (int i=0;i<n-1;i++){
+            for (int j=0;j<n-i-1;j++) {
+                if (arr[j] < arr[j +1]) {
+                    int temp = arr[j ];
+                    arr[j ] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array in Decreasing order");
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
