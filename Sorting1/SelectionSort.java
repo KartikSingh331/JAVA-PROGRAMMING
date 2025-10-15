b03296bec62abf9942bb80d5e1a2f6de3b0e546c
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-16 00:06:15 +0530

    Add files via upload

---
+++ Sorting1/SelectionSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+public class SelectionSort {
+    public static void print(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr={10,-8,25,78,5,20};
+        int n=arr.length;
+        for (int i=0;i<n-1;i++){
+            int min=Integer.MAX_VALUE;
+            int mindx=-1;
+            for (int j=i;j<n;j++){
+                if(arr[j]<min){
+                    min=arr[j];
+                    mindx=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[mindx];
+            arr[mindx]=temp;
+        }
+print(arr);
+    }
+        }
+
+
