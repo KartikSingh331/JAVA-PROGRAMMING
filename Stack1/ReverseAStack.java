af3bef3dd621a8f213a7ce2bc56b24deda7685b8
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-22 07:24:10 +0530

    Add files via upload

---
+++ Stack1/ReverseAStack.java
@@ -0,0 +1,21 @@
+package Stack;
+import java.util.*;
+public class ReverseAStack {
+    public static void main(String[] args) {
+        Stack<Integer> st_Original = new Stack<>();
+        st_Original.push(58);
+        st_Original.push(62);
+        st_Original.push(68);
+        st_Original.push(99);
+        st_Original.push(8);
+        System.out.println("Original Stack: "+st_Original);
+        Stack<Integer> st_Reversed = new Stack<>();
+        while (!st_Original.isEmpty()) {
+            int element = st_Original.pop();
+            st_Reversed.push(element);
+               // Or
+//            st_Reversed.push(st_Original.pop());
+        }
+        System.out.println("Reversed Stack; "+st_Reversed);
+    }
+}
