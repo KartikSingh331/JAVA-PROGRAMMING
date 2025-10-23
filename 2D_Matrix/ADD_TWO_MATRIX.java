cd75e6f117747fb972ce181639e9d83bce2c2964
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-23 08:37:26 +0530

    Add files via upload

---
+++ 2D_Matrix/ADD_TWO_MATRIX.java
@@ -0,0 +1,25 @@
+package TWO_D_ARRAY;
+
+public class ADD_TWO_MATRIX {
+    public static void main(String[] args) {
+        int[][] a={{5,7,8},{78,85,96}};
+        int[][] b={{8,2,1},{5,6,3}};
+        int m=a.length;
+        int n=b[0].length;
+        int[][] res=new int[m][n];
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    res[i][j] = a[i][j] + b[i][j];
+                }
+            }
+
+            for (int i = 0; i <m ; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(res[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
