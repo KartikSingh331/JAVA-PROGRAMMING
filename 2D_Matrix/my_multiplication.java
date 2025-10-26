069f877f5fec77ae64e230079bf0844fcbd83d13
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-26 07:57:36 +0530

    Add files via upload

---
+++ 2D_Matrix/my_multiplication.java
@@ -0,0 +1,19 @@
+package TWO_D_ARRAY;
+import java.util.*;
+public class my_multiplication {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the elements of matrix a: ");
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int m1=sc.nextInt();
+        int n1=sc.nextInt();
+        int[][] a=new int[m][n];
+        for (int i=0;i< a.length;i++){
+            for (int j=0;j<a[0].length;j++){
+
+            }
+
+        }
+    }
+}
