a1c88b0e14dc1feb1b11fb736d2114cd7a38e5de
KartikSingh331 <kartiksinh551@gmail.com>
2025-10-30 19:42:55 +0530

    Add files via upload

---
+++ 2D_Matrix/Transpose_withoutUSING_new_MATRIX.java
@@ -0,0 +1,24 @@
+package TWO_D_ARRAY;
+
+public class Transpose_withoutUSING_new_MATRIX {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+        int m= arr.length;
+        int n=arr[0].length;
+
+        for (int i = 0; i <m ;i++) {
+            for (int j = 0; j <=i ; j++) {
+             int  temp=arr[i][j];
+               arr[i][j]=arr[j][i];
+               arr[j][i]=temp;
+            }
+        }
+        for (int j= 0; j <m ; j++) {
+            for (int i = 0; i <n ; i++) {
+                System.out.print(arr[j][i]+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
