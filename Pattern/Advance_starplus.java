e1be927cf39b792dccaf350244b281a0c0642e5e
KartikSingh331 <kartiksinh551@gmail.com>
2025-11-01 23:26:15 +0530

    Update Advance_starplus.java

--- Pattern/Advance_starplus.java
+++ Pattern/Advance_starplus.java
@@ -5,6 +5,7 @@ public class Advance_starplus {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int m= sc.nextInt();
+        
         for (int i=1;i<=m;i++){
             int mid=m/2+1;
             for (int j=1;j<=m;j++){
@@ -19,4 +20,5 @@ public class Advance_starplus {
         }
     }
 }
+
 
