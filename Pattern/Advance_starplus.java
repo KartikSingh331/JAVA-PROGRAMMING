a504790472062da49a8da16f5cde5fcd7cacd21f
KartikSingh331 <kartiksinh551@gmail.com>
2025-11-01 23:25:52 +0530

    Update Advance_starplus.java

--- Pattern/Advance_starplus.java
+++ Pattern/Advance_starplus.java
@@ -1,6 +1,7 @@
 package Pattern;
 import java.util.Scanner;
 public class Advance_starplus {
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int m= sc.nextInt();
@@ -18,3 +19,4 @@ public class Advance_starplus {
         }
     }
 }
+
